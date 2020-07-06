a4feebf964f065f76e48c379710e542bf9b79b37
Adhika Gunadarma <adhikagunadarma@gmail.com>
2020-07-06 20:11:43 +0700

    Initial Commit

---
+++ simple-crud-contact-jenius/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  simple-crud-contact-jenius
+//
+//  Created by Josephine Fransisca on 06/07/20.
+//  Copyright © 2020 Adhika gunadarma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
