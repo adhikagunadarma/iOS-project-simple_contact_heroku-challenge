9d2a046587f84486732a89654d67248fa36c5898
Adhika Gunadarma <adhikagunadarma@gmail.com>
2020-07-06 21:41:44 +0700

    model dan api service

--- simple-crud-contact-jenius/service/APIService.swift
+++ simple-crud-contact-jenius/service/APIService.swift
@@ -7,3 +7,30 @@
 //
 
 import Foundation
+
+
+class APIService{
+    private let baseURL = "https://simple-contact-crud.herokuapp.com/"
+    
+    public func getAllContacts(){
+        
+    }
+    
+    public func getContact(_ id : String){
+        
+    }
+    
+    public func addNewContact(_ model : Contact){
+        
+    }
+    
+    public func editContact(_ model : Contact){
+        
+    }
+    
+    public func deleteContact(_ id : String){
+        
+    }
+    
+    
+}
